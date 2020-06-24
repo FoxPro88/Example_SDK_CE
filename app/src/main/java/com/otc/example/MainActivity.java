7bbf02c99095e3eefc608631e511a81f65b621af
Farid <farid_gamarra@hotmail.es>
2020-06-24 08:07:19 -0500

    Update integration

--- app/src/main/java/com/otc/example/MainActivity.java
+++ app/src/main/java/com/otc/example/MainActivity.java
@@ -4,26 +4,34 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.Scroller;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interceptors.HttpLoggingInterceptor;
+import com.androidnetworking.interfaces.ParsedRequestListener;
 import com.androidnetworking.interfaces.StringRequestListener;
 import com.otc.sdk.commerce.OtcApplication;
+import com.otc.sdk.commerce.data.model.request.SessionTokenRequest;
 import com.otc.sdk.commerce.data.model.request.authorization.AuthorizationRequest;
 import com.otc.sdk.commerce.data.model.request.authorization.Device;
 import com.otc.sdk.commerce.data.model.request.authorization.Header;
 import com.otc.sdk.commerce.data.model.request.authorization.Merchant;
 import com.otc.sdk.commerce.data.model.request.authorization.Order;
+import com.otc.sdk.commerce.data.model.response.SessionTokenResponse;
 import com.otc.sdk.commerce.presentation.custom.PaymentViewCustom;
 import com.otc.sdk.commerce.util.Utils;
 
@@ -44,10 +52,19 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
+    private static String ENDPOINT = "apiqa.ecore.mobi";
+    private static String TENANT = "lpg";
+    private static String MERCHANT = "40116061";
+
+    private String mAmount = "";
+    private String mPurchaseNumber = "";
+
+
     EditText etAmount;
     EditText etPurchaseNumber;
     Button btnPay;
     RelativeLayout viewLoading;
+    OtcApplication OTC_SDK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         init();
     }
 
-    private void init(){
+    private void init() {
 
         viewLoading = findViewById(R.id.loading_view);
 
@@ -77,23 +94,23 @@ public class MainActivity extends AppCompatActivity {
 
     private void payment() {
 
-        String amount = etAmount.getText().toString();
-        if (amount == "") {
+        mAmount = etAmount.getText().toString();
+        if (mAmount == "") {
             etAmount.setError("Ingresar un monto a pagar.");
             return;
         }
 
-        String purchaseNumber = etPurchaseNumber.getText().toString();
-        if (purchaseNumber == "") {
+        mPurchaseNumber = etPurchaseNumber.getText().toString();
+        if (mPurchaseNumber == "") {
             etPurchaseNumber.setError("Ingresar un número de pedido.");
             return;
         }
 
-        AccessToken(amount, purchaseNumber);
+        ApiSecurity();
 
     }
 
-    private void AccessToken(final String amount, final String purchaseNumber) {
+    private void ApiSecurity() {
 
         viewLoading.setVisibility(View.VISIBLE);
 
@@ -108,8 +125,11 @@ public class MainActivity extends AppCompatActivity {
             }
         }).build();
 
+        String path = "https://apiqa.ecore.mobi/api.security/v2/lpg/security/accessToken";
+        String[] values = {ENDPOINT, TENANT, MERCHANT};
+
         AndroidNetworking.enableLogging(HttpLoggingInterceptor.Level.BODY);
-        AndroidNetworking.get("https://apiqa.ecore.mobi/api.security/v2/lpg/security/accessToken")
+        AndroidNetworking.get(MessageFormat.format(path, values))
                 .setOkHttpClient(client)
                 .setTag(this)
                 .setPriority(Priority.HIGH)
@@ -120,27 +140,7 @@ public class MainActivity extends AppCompatActivity {
 
                         viewLoading.setVisibility(View.GONE);
 
-                        //custom
-                        PaymentViewCustom sdkCustom = new  PaymentViewCustom();
-                        sdkCustom.setInputLogo(R.drawable.logo);
-                        sdkCustom.setButtonPayColor(R.color.amber_600);
-
-                        OtcApplication.setApplicationContext(MainActivity.this);
-                        //default
-                        //OtcApplication.setCurrency(Currency.USD)
-                        //OtcApplication.setLanguage(Language.SPANISH)
-                        //OtcApplication.setChannel(Channel.MOBILE)
-                        OtcApplication.setEndpoint("apiqa.ecore.mobi");
-                        OtcApplication.setTenantId("lpg");
-                        OtcApplication.setMerchantId("40116061");
-                        OtcApplication.setSecurityToken(response); //jwt
-                        //custom
-                        OtcApplication.setCustom(sdkCustom);
-
-                        OtcApplication otcSdk = OtcApplication.getInstance();
-
-                        otcSdk.openPaymentActivity(MainActivity.this, amount, purchaseNumber);
-
+                        ApiRegisterOrder(response);
                     }
 
                     @Override
@@ -160,10 +160,76 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void Authorization(String data) {
+    private void ApiRegisterOrder(final String accessToken) {
+
+        viewLoading.setVisibility(View.VISIBLE);
+
+
+
+        SessionTokenRequest request = new SessionTokenRequest();
+        request.setChannel(OtcApplication.getChannel().name());
+        request.setCurrency(OtcApplication.getCurrency().name());
+        request.setAmount(Utils.parseAmount(mAmount));
+
+        String path = "https://{0}/api.ecommerce/v3/{1}/token/session/{2}";
+        String[] values = {ENDPOINT, TENANT, MERCHANT};
+
+        AndroidNetworking.post(MessageFormat.format(path, values))
+                .setTag(this)
+                .addHeaders("Authorization", accessToken)
+                .addHeaders("Content-Type", "application/json")
+                .addApplicationJsonBody(request)
+                .setPriority(Priority.HIGH)
+                .build()
+                .getAsObject(SessionTokenResponse.class, new ParsedRequestListener<SessionTokenResponse>() {
+                    @Override
+                    public void onResponse(SessionTokenResponse response) {
+                        //-------------------------------- SDK OTC -------------------------------------------------
+                        PaymentViewCustom sdkCustom = new PaymentViewCustom();
+                        sdkCustom.setInputLogo(R.drawable.logo);
+                        sdkCustom.setButtonPayColor(R.color.amber_600);
+
+                        OtcApplication.setApplicationContext(MainActivity.this);
+                        //default
+                        //OtcApplication.setCurrency(Currency.USD)
+                        //OtcApplication.setLanguage(Language.SPANISH)
+                        //OtcApplication.setChannel(Channel.MOBILE)
+                        OtcApplication.setEndpoint(ENDPOINT);
+                        OtcApplication.setTenantId(TENANT);
+                        OtcApplication.setMerchantId(MERCHANT);
+                        OtcApplication.setSecurityToken(accessToken);
+                        //custom
+                        OtcApplication.setCustom(sdkCustom);
+
+                        OTC_SDK = OtcApplication.getInstance();
+                        //------------------------------------------------------------------------------------------
+                        String sessionToken = response.getSessionKey();
+
+                        OTC_SDK.openPaymentActivity(MainActivity.this, mAmount, mPurchaseNumber, sessionToken);
+
+                    }
+
+                    @Override
+                    public void onError(ANError anError) {
+
+                        viewLoading.setVisibility(View.GONE);
+
+                        Log.i(TAG, "onError: " + anError.getErrorCode());
+                        Log.i(TAG, "onError: " + anError.getErrorDetail());
+                        Log.i(TAG, "onError: " + anError.getErrorBody());
+                        Log.e(TAG, "onError: ", anError);
+
+                        Toast.makeText(MainActivity.this, "Error : Revisar el log....", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+
+    }
+
+    private void ApiAuthorization(String data) {
 
         try {
-            JSONObject json_contact =new JSONObject(data);
+            JSONObject json_contact = new JSONObject(data);
             String transactionToken = json_contact.getString("tokenId");
             String expiration = json_contact.getString("expiration");
 
@@ -198,13 +264,13 @@ public class MainActivity extends AppCompatActivity {
             String tenant = "lpg";
 
             String path = "https://{0}/api.authorization/v3/{1}/authorize";
-            String[] values = { endpoint, tenant };
+            String[] values = {endpoint, tenant};
 
 
             viewLoading.setVisibility(View.VISIBLE);
 
             AndroidNetworking.post(MessageFormat.format(path, values))
-            .setTag(this)
+                    .setTag(this)
                     .addHeaders("Authorization", OtcApplication.getSecurityToken())
                     .addHeaders("Content-Type", "application/json")
                     .addApplicationJsonBody(authorizationRequest)
@@ -217,6 +283,8 @@ public class MainActivity extends AppCompatActivity {
                             viewLoading.setVisibility(View.GONE);
 
                             Log.i(TAG, "onResponse: " + response);
+
+                            dialogResult(MainActivity.this, response);
                         }
 
                         @Override
@@ -226,6 +294,8 @@ public class MainActivity extends AppCompatActivity {
 
                             Log.i(TAG, "onError: " + anError.getErrorDetail());
 
+                            dialogResult(MainActivity.this, anError.getErrorDetail());
+
                         }
                     });
 
@@ -234,7 +304,24 @@ public class MainActivity extends AppCompatActivity {
             Log.e(TAG, "Authorization: ", e);
         }
 
+    }
 
+    private void dialogResult(Activity context, String msg){
+        AlertDialog dialog = new AlertDialog.Builder(context)
+                .setTitle("OTC-DEMO")
+                .setMessage(msg)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog1, int which) {
+                        dialog1.dismiss();
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .show();
+        TextView textView = dialog.findViewById(android.R.id.message);
+        textView.setScroller(new Scroller(context));
+        textView.setVerticalScrollBarEnabled(true);
+        textView.setMovementMethod(new ScrollingMovementMethod());
     }
 
 
@@ -251,18 +338,18 @@ public class MainActivity extends AppCompatActivity {
 
                     resultString = data.getExtras().getString(OtcApplication.KEY_SUCCESS);
 
-                            //show result
-                            //Util.dialogResult(this, returnString)
+                    //show result
+                    //Util.dialogResult(this, returnString)
 
-                            Authorization(resultString);
+                    ApiAuthorization(resultString);
 
                 } else {
 
                     resultString = data.getExtras().getString(OtcApplication.KEY_ERROR);
-                            //show result
+                    //show result
                     Toast.makeText(this, resultString, Toast.LENGTH_SHORT).show();
                 }
-            }else {
+            } else {
                 Toast.makeText(this, "Cancelado....", Toast.LENGTH_SHORT).show();
             }
 
